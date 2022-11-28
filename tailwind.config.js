5cac5a4347cd013507767d263866c7485881eefb
rythmn1111 <82413584+rythmn1111@users.noreply.github.com>
2022-11-28 19:22:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [ "./src/**/*.tsx" ],
+  theme: {
+    extend: {
+      colors:{
+        tabMain: {
+          500: "rgb(219,82,77)",
+          400: "#db524d80"
+        },
+        tabShortTime: {
+          500: "rgb(70,142,145)",
+        },
+        tabLongTime:{
+          500: "rgb(67,126,168)",
+        },
+      },
+      fontFamily: {
+        mPlus: ['"M PLUS Rounded 1c"', 'sans-serif'],
+      },
+      borderRadius: {
+        md: '4px'
+      }
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar')
+  ],
+  variants: {
+    scrollbar: ['rounded']
+  }
+}
